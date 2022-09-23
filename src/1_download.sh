8c96d792dab9d7d423de44a555e4296a581428c9
Jose A. Jaramillo-Villegas <jaljavi@icloud.com>
2022-09-23 18:24:54 -0400

    wget quiet progress

--- src/1_download.sh
+++ src/1_download.sh
@@ -10,5 +10,5 @@ echo "Download path: $downloadpath"
 mkdir -p "$downloadpath"
 cd $downloadpath;
 ftpbase="ftp://rsync.wwpdb.org/pub/pdb/data/structures/divided/pdb/"
-wget -r -A ent.gz -nH --cut-dirs=6 -nc $ftpbase
+wget -r -A ent.gz -nH -q --show-progress --cut-dirs=6 -nc $ftpbase
 echo "Download done."
