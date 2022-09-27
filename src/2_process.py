8a64da871dbaeddf30e9f28e28db093186c7af82
Alex Borrel <a.borrel@gmail.com>
2022-09-27 11:23:15 -0400

    Fix function to convert PDB header to dataframe

--- src/2_process.py
+++ src/2_process.py
@@ -23,7 +23,6 @@ def process_pdb(f_in, d_f):
     for k in structure.header.keys():
         d_temp[k] = structure.header[k]
     
-    print(d_temp)
     if d_f != {}:
         n_data_in = len(d_f[list(d_f.keys())[0]])
     else:
@@ -31,31 +30,35 @@ def process_pdb(f_in, d_f):
 
     # l_inter
     l_union = list(set(list(d_temp.keys())) | set(list(d_f.keys())))
+    l_inter = list(set(list(d_temp.keys())) & set(list(d_f.keys())))
     
     for k_union in l_union:
-        if k_union in list(d_f.keys()):
+        if k_union in l_inter:
             d_f[k_union].append(d_temp[k_union])
-        else:
+        elif k_union in list(d_temp.keys()):
             d_f[k_union] = ["NA"] * n_data_in
             d_f[k_union].append(d_temp[k_union])
+        else:
+            d_f[k_union].append("NA")
 
 
 
 ## MAIN
-p_dir_download = "/mnt/nvme/aborrel/git/protein-data-bank/download/data/"
-
-
-# open pdb
-p_dir_PDB = p_dir_download + "structures/all/pdb/"
+p_dir_PDB = "/mnt/md0/biobricks/pdb/download/"
 
 # test on the first file
-l_fildir = listdir(p_dir_PDB)
+l_dirPDB = listdir(p_dir_PDB)
 
 d_f = {}
-for f_pdb in l_fildir[:10]:
-    pf_PDB = p_dir_PDB + f_pdb
-    process_pdb(pf_PDB, d_f)
+count = 0
+for dirpdb in l_dirPDB[:10]:
+    l_filepdb = listdir(p_dir_PDB + dirpdb)
+    for f_pdb in l_filepdb:
+        pf_PDB = p_dir_PDB + dirpdb + "/" + f_pdb
+        count = count + 1
+        process_pdb(pf_PDB, d_f)
 
+print(count)
 #print(d_f)
 df_out = pd.DataFrame(data=d_f)
 
